4f769455c7ac9df1444b3fcdc08500cf3f1bf784
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2019-05-01 21:31:18 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,21 +1,9 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    @Override
-    List<Task> findAll();
-
-    @Override
-    Task findOne(Long id);
-
-    @Override
-    Task save( Task task);
-
-    @Override
-    void delete(Long aLong);
 }
