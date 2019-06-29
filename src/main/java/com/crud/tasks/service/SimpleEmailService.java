128ccb964630279a59f269acc1f57e5270bbe8ad
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2019-06-29 22:33:26 +0200

    23.5

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -10,7 +10,6 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Service
 public class SimpleEmailService {
